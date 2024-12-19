30652f2754d378ff3a7485ca0378453b856f1cab
Franco <franco_tigres18@hotmail.com>
2024-12-19 09:29:36 -0600

    style|RegisterView basic style

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,3 +1,11 @@
 <script setup></script>
 
-<template>RegisterView</template>
+<template>
+  <h1 class="text-title">Crear Cuenta</h1>
+
+  <nav class="mt-10">
+    <RouterLink :to="{ name: 'login' }" class="text-center text-white text-lg block"
+      >¿Ya tienes una cuenta? Inicia sesión</RouterLink
+    >
+  </nav>
+</template>
