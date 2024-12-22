f09d3125e00dccca562d7681d9a2730087aa111f
Franco <franco_tigres18@hotmail.com>
2024-12-21 19:53:55 -0600

    fix|auth routes

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,19 +1,17 @@
-export default [
-  {
-    path: '/auth',
-    name: 'auth',
-    component: () => import('@/views/auth/AuthLayout.vue'),
-    children: [
-      {
-        path: 'registrar',
-        name: 'register',
-        component: () => import('@/views/auth/RegisterView.vue'),
-      },
-      {
-        path: 'login',
-        name: 'login',
-        component: () => import('@/views/auth/LoginView.vue'),
-      },
-    ],
-  },
-]
+export default {
+  path: '/auth',
+  name: 'auth',
+  component: () => import('@/views/auth/AuthLayout.vue'),
+  children: [
+    {
+      path: 'registrar',
+      name: 'register',
+      component: () => import('@/views/auth/RegisterView.vue'),
+    },
+    {
+      path: 'login',
+      name: 'login',
+      component: () => import('@/views/auth/LoginView.vue'),
+    },
+  ],
+}
--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      ...AuthRoutes[0],
+      ...AuthRoutes,
     },
   ],
 })
