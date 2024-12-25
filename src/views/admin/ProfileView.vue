91f2b7c6d1068c10c2838a9a4a11ca1cd24e4f90
Franco <franco_tigres18@hotmail.com>
2024-12-25 13:13:08 -0600

    style|profile design form

--- src/views/admin/ProfileView.vue
+++ src/views/admin/ProfileView.vue
@@ -1,5 +1,26 @@
 <script setup></script>
 
 <template>
-  <h1>Profile View</h1>
+  <FormKit
+    id="profile-form"
+    type="form"
+    :actions="false"
+    incomplete-message="No se pudo enviar, revisa los mensajes"
+    classes="bg-white p-10 rounded-lg space-y-5"
+  >
+    <FormKit
+      type="text"
+      label="Handle"
+      name="handle"
+      placeholder="Escribe un handle"
+      validation="required"
+      :validation-messages="{ required: 'El handle es obligatorio' }"
+    />
+
+    <FormKit type="text" label="Descripción" name="description" placeholder="Tu descripción" />
+
+    <FormKit type="file" label="Imagen" name="image" accept=".png,.jpg.jpeg" />
+
+    <FormKit type="submit">Guardar Cambios</FormKit>
+  </FormKit>
 </template>
